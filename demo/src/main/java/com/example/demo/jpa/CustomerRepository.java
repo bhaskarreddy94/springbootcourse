692cd662cc6be1c58ee927aca796f0715e7ea12e
reddy <bhaskar>
2019-12-28 10:04:52 +0530

    vooo

--- demo/src/main/java/com/example/demo/jpa/CustomerRepository.java
+++ demo/src/main/java/com/example/demo/jpa/CustomerRepository.java
@@ -7,6 +7,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.demo.model.Customer;
 
 public interface CustomerRepository  extends JpaRepository<Customer, Long>{
+	
+	
 List<Customer> findByFirstName(String firstname);
 List<Customer> findByLastName(String lastName);
 }

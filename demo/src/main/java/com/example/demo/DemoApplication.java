5b5ee5fda85b361df2b0633540fa97e73f035a63
Thirupathi Bontha <bontha.thirupathi@s-force.org>
2020-02-08 22:22:19 +0530

    add comments for the main class

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -2,7 +2,7 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//this main class for the project
 @SpringBootApplication
 public class DemoApplication {
 	public static void main(String[] args) {

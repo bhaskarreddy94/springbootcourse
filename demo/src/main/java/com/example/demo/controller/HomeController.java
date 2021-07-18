621d3d3e992ccd4d008bd2a13a9e5c736128e881
91955 <bhaskarreddy94>
2021-07-18 20:48:08 +0530

    JSP and MVC

---
+++ demo/src/main/java/com/example/demo/controller/HomeController.java
@@ -0,0 +1,40 @@
+package com.example.demo.controller;
+
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.example.demo.model.Login;
+import com.example.demo.model.User;
+import com.example.demo.service.UserService;
+
+
+@Controller
+public class HomeController {
+
+  @Autowired
+  public UserService userService;
+
+  @GetMapping("/")
+  public String home(Map<String, Object> map) {
+    return "home";
+  }
+
+  @GetMapping("/showLoginPage")
+  public String showLoginPage(ModelMap model) {
+    model.addAttribute(new Login());
+
+    return "login";
+  }
+
+  @GetMapping("/showRegistrationPage")
+  public String showRegistrationPage(ModelMap model) {
+    model.addAttribute(new User());
+
+    return "registration";
+  }
+
+}
---
+++ demo/src/main/java/com/example/demo/service/UserService.java
@@ -0,0 +1,21 @@
+package com.example.demo.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.example.demo.jpa.UserRepository;
+import com.example.demo.model.Login;
+import com.example.demo.model.User;
+@Component
+public class UserService {
+	@Autowired
+	  public UserRepository userRepo;
+
+	  public void addUser(User user) {
+	    userRepo.save(user);
+	  }
+
+	  public User validateUser(Login login) {
+	    return userRepo.findById(login.getUsername());
+	  }
+}
